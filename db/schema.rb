5ba04ac7fee76ccd9debb53cfd31e6a0a5bd2568
Jose Moreno <josemoreno@Joses-iMac.attlocal.net>
2020-02-19 18:33:16 -0800

    Added user model files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123054649) do
+ActiveRecord::Schema.define(version: 20200220023231) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20190123054649) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
